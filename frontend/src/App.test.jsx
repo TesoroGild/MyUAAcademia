05179542916ed3692592785cbc55ec395050278e
Vito <gilboss85@gmail.com>
2025-03-22 23:46:32 -0400

    adding first test

---
+++ frontend/src/App.test.jsx
@@ -0,0 +1,13 @@
+import {render, screen} from '@testing-library/react';
+import { MemoryRouter , BrowserRouter } from "react-router-dom"; 
+import App from './App';
+import '@testing-library/jest-dom'; 
+
+test("affiche la page d'accueil", () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Code Permanent :")).toBeInTheDocument();
+  });
--- frontend/src/components/home/home.spec.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "./home";
-
-test("affiche la page d'acceuil", () => {
-  render(<Home />);
-  expect(screen.getByText("News")).toBeInTheDocument();
-});
