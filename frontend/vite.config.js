1246ce9711d15f4bdda4282eb89cb6c316ee2b95
Vito <gilboss85@gmail.com>
2025-03-23 14:15:57 -0400

    adding build dir; changing build command; changing deploy package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./",
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true
+  },
   test: {
     globals: true,
     environment: "jsdom"
