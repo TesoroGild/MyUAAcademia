32de9db898f2e3fecee1759dc488e2e2ea9bcc99
Vito <gilboss85@gmail.com>
2025-03-24 16:30:15 -0400

    changing base root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: "./",
-  base: "./", // ou "/wwwroot/"
+  base: "/wwwroot/",
   build: {
     outDir: "./dist",
     emptyOutDir: true
