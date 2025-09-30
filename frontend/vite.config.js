365a3df0767f3c4315a9af198a98d7f2fdff57af
Vito <gilboss85@gmail.com>
2025-09-30 11:33:05 -0400

    trying putting app on https.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from "vite-plugin-mkcert";
-
-//const backend_url = import.meta.env.VITE_PORT || 8080;
+//import mkcert from "vite-plugin-mkcert";
+import fs from 'fs'
+import path from 'path'
 
 
 // https://vitejs.dev/config/
@@ -10,7 +10,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    plugins: [react(), mkcert()],
+    //, mkcert()
+    plugins: [react()],
     root: "./",
     base: "/",
     build: {
@@ -23,11 +24,18 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       https: {
-        cert: env.VITE_SSL_CRT_FILE,
-        key: env.VITE_SSL_KEY_FILE
+        cert: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_CERT_FILE)),
+        key: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_KEY_FILE)),
       },
       port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 8080,
-      host: true
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: true,
+        },
+      }
     }
   }
 })
