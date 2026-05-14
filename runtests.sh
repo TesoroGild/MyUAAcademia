6d1f2666bc738621eb4cd1272ec43a68c07b796e
Vito <gilboss85@gmail.com>
2026-05-14 12:06:32 -0400

    Formating script and add sleep after executions.

--- runtests.sh
+++ runtests.sh
@@ -3,19 +3,31 @@ set -e
 
 echo "\n=== Lint Frontend ==="
 cd frontend/app && npm run lint
+sleep 2
+echo "=== Fin Lint F ==="
 
 echo "\n=== Tests Frontend ==="
 npm run test:run -- --reporter=verbose && CI=true npm run test:e2e
+sleep 2
+echo "=== Fin Tests F ==="
 
 echo "\n=== Build Frontend ==="
 npm run build
+sleep 2
+echo "=== Fin Build F ==="
 cd ../../
 
 echo "\n=== Build Backend ==="
 cd backend && dotnet build
+sleep 2
+echo "=== Fin Build B ==="
 
 echo "\n=== Formatage Backend ==="
 dotnet format --verify-no-changes
+sleep 2
+echo "=== Fin formatage ==="
 
 echo "\n=== Tests Backend ==="
 dotnet test --logger "console;verbosity=detailed"
+sleep 2
+echo "=== Fin Tests B ==="
