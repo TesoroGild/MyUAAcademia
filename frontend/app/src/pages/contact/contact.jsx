d241bd148365d52e755d0384fa1a63c2634fc5e9
TesoroGild <88209593+TesoroGild@users.noreply.github.com>
2026-08-04 02:53:00 -0400

    Update contact page content
    Added additional text to the contact page.

--- frontend/app/src/pages/contact/contact.jsx
+++ frontend/app/src/pages/contact/contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (<>
         <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus sapien, ornare a efficitur finibus, luctus ut metus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus sapien, ornare a efficitur finibus, luctus ut metus. Cras eu mi vel eros hendrerit euismod.
         </div>
     </>)
 }
