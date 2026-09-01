fd32718d7ea56df9538df383bb9862b03717cf4b
Vito <gilboss85@gmail.com>
2026-09-01 12:38:46 -0400

    Set env for states managment.

---
+++ terraform/terraform_bootstrap/main.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "myua-prod-tfstate-bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tflock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
