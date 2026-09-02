6efbee8b6e8c52d0450d7a9a330d10582f1a4d04
Vito <gilboss85@gmail.com>
2026-09-02 10:59:18 -0400

    Add testatuh helpers to be reusable.

---
+++ backend/tests/Helpers/TestAuthHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace tests.Helpers
+{
+    public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public const string SchemeName = "TestScheme";
+
+        public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder)
+            : base(options, logger, encoder)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.TryGetValue("Test-Role", out var role))
+                return Task.FromResult(AuthenticateResult.NoResult());
+
+            var claims = new List<Claim>
+        {
+            new Claim("permanentCode", "TestUser"),
+            new Claim(ClaimTypes.Role, role.ToString())
+        };
+            var identity = new ClaimsIdentity(claims, SchemeName);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, SchemeName);
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
